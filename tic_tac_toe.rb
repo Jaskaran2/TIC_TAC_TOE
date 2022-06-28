212cb84b42ab9ae37d7bebdeab394436711ef924
Jaskaran2 <60808292+Jaskaran2@users.noreply.github.com>
2022-06-28 08:54:26 +0530

    Update tic_tac_toe.rb

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,22 +1,21 @@
-def play_the_game
+require 'colorize'
+
     
-    board=[[1,2,3],[4,5,6],[7,8,9]]
-    win=false
-    user_x=false
-    user_o=true
-    chance =0
-    inputs=[]
+board=[[1,2,3],[4,5,6],[7,8,9]]
+win=false
+$user_x=false
+$user_o=true
+chance =0
+inputs=[]
 
 
 #display the board
 def display_board(board)
-    z=[]
     for i in 0..2
         for j in 0..2
-            z<< board[i][j]
+            print board[i][j].to_s+"   "
     end
-    puts z.join("   ")
-    z=[]
+    puts " "
     puts " "
 end
 end
@@ -37,12 +36,16 @@ def find_pos(board,ele)
 end
 
 
-#setting the user symbol
-def mark_o_x(i,user,board,user_x,user_o)
-    if user_x==true
-        board[i[0]][i[1]]=user
+#setting the user symbol and setting the player
+def mark_o_x(i,user,board)
+    if $user_x==true
+        board[i[0]][i[1]]=user.green
+        $user_x=false
+        $user_o=true
     else 
-        board[i[0]][i[1]]=user
+        board[i[0]][i[1]]=user.yellow
+        $user_x=true
+        $user_o=false
     end
 end
 
@@ -73,20 +76,20 @@ end
 
 #Game logic
 while win==false
+    
     chance=chance+1
     index=[]
     display_board(board)
     correct_response=1
-    puts "Player #{user_x==true ? "x": "o"} choose from remaining positions:"
+    puts "Player #{$user_x==true ? "X": "O"} choose from remaining positions:".blue
     respond=gets.chomp.to_i
-
 
     
     #Making Sure User selects only the position which is not pre occupied or error input
     if inputs.include?respond or respond>9 or respond==0
         correct_response=0
         while correct_response==0
-            puts "Wrong inout!! Try again"
+            warn "Wrong input!! Try again".red
             respond=gets.chomp.to_i
             if inputs.include?respond or respond>9  or respond==0
                 correct_response=0
@@ -100,37 +103,19 @@ while win==false
     end
     puts " "
    
-
+    #Getting the index where the user selected
     index=find_pos(board,respond)
-    mark_o_x(index, user_x==true ? "x": "o",board,user_x,user_o)
+    mark_o_x(index, $user_x==true ? "X": "O",board)
     
     #Checking for end game logic
     if end_game(board)==true
         win=true
-        puts "Player  #{user_x==true ? "x": "o"} won" 
+        puts "Player  #{$user_x==true ? "O" : "X"} won".green
         display_board(board)
       elsif chance==9
         win=true
-        puts "Draw"
+        puts "Draw".green
         display_board(board)
       end
-
-    # setting user      
-    if user_x==true
-        user_x=false
-        user_o=true
-    else
-        user_x=true
-        user_o=false
-    end
-    
    end
-end
-
-play_the_game
-
-
-
-
-
 
